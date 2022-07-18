d9805bfd2d1ee9d21aeaf2475b77062e41fb333f
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-07-17 23:13:56 -0300

    feat: prevented from updating like by repository update

--- src/index.js
+++ src/index.js
@@ -30,17 +30,26 @@ app.post("/repositories", (request, response) => {
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const {title, url, techs} = request.body;
+  // const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  // repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex < 0) {
-    return response.status(404).json({ error: "Repository not found" });
+  // if (repositoryIndex === -1) {
+  //   return response.status(404).json({ error: "Repository not found" });
+  // }
+
+  // const repository = { ...repositories[repositoryIndex], ...updatedRepository };
+  // repositories[repositoryIndex] = repository;
+
+  const repository = repositories.find(repository => repository.id === id)
+  if (!repository) {
+    return response.status(404).json({ error: 'Repository not found'})
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
-
-  repositories[repositoryIndex] = repository;
+  repository.title = title
+  repository.url = url
+  repository.techs = techs
 
   return response.json(repository);
 });
@@ -64,13 +73,13 @@ app.post("/repositories/:id/like", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex < 0) {
+  if (repositoryIndex === -1) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  return response.json(likes);
 });
 
 module.exports = app;
